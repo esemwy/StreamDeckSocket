8f4555a69e85e67a3c1ecb8b30a5f557f1f76e5d
Brian <38627248+Windamyre@users.noreply.github.com>
2023-01-02 22:41:53 -0500

    Update actionserver.h
    Updated to support Daz Studio actions added by Esemwy
    Added Stream Deck Plus dial support

--- actionserver.h
+++ actionserver.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "qhttpserver/qhttpserverfwd.h"
+#include "dzfloatproperty.h"
 
 #include <QtCore/QObject>
 #include <dzjsonwriter.h>
@@ -15,9 +16,14 @@ public:
 	ActionServer();
 
 private:
-	void enumerateActions(DzJsonWriter *json);
-	int executeAction(QString action);
+	void enumerateActions(DzJsonWriter* json);
+	void enumerateCustomActions(DzJsonWriter* json);
+	void enumerateDAZActions(DzJsonWriter* json);
+	int executeCustomAction(QString action);
+	int executeDAZAction(QString action);
+
+	float ActionServer::getPropertyAsPercent(DzFloatProperty* prop);
 
 public slots:
-	void handleRequest(QHttpRequest *req, QHttpResponse *resp);
+	void handleRequest(QHttpRequest* req, QHttpResponse* resp);
 };
